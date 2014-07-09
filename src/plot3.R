b2fa2858ac771269ea7474e1b2be5044a1944ee4
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-07-09 15:11:00 -0500

    Plot 3 done.

---
+++ src/plot3.R
@@ -0,0 +1,19 @@
+source("./src/load_data.R")
+
+household.power.consumption <- load.data()
+
+file.name <- "./plots/plot3.png"
+if (file.exists(file.name)) {
+        file.remove(file.name)
+}
+png(file = file.name, width = 480, height = 480)
+with(household.power.consumption, 
+     plot(DateTime, Sub_metering_1, type = "l", xlab="", 
+          ylab="Energy sub metering"))
+with(household.power.consumption, 
+     points(DateTime, Sub_metering_2, type = "l", col = "red"))
+with(household.power.consumption, 
+     points(DateTime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
